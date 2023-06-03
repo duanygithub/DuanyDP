ed32df16321c79fbf3f9f5a436a6da0e8a3eee07
duanygithub <duanyuxuan5566232@163.com>
2023-06-03 16:56:56 +0800

    v1.0.2

---
+++ DuanyDP/Pages/develop/log.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DuanyDP.Pages.develop
+{
+    public class logModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
